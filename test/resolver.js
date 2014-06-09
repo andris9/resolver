eb2f7aef9260ec28e3fa787b88e85828fd1a2b61
Andris Reinman <andris@kreata.ee>
2014-06-09 15:03:34 +0300

    Bumped to v0.1.12

--- test/resolver.js
+++ test/resolver.js
@@ -1,55 +1,58 @@
-var testCase = require('nodeunit').testCase,
-    resolver = require("../index");
+'use strict';
 
-exports["Url tests"] = {
-    "Check FeedBurner URl": function(test){
-        resolver.resolve("http://feedproxy.google.com/~r/AndrisReinman/~3/pPcknhJRb_I/15668406504", function(err, url){
+var resolver = require('../index');
+
+exports['Url tests'] = {
+    'Check FeedBurner URl': function(test) {
+        resolver.resolve('http://feedproxy.google.com/~r/AndrisReinman/~3/Cwi_bAR2Ljc/', function(err, url, fileName, contentType) {
             test.ifError(err);
-            test.equal(url, "http://www.andrisreinman.com/post/15668406504/eesti-saitide-kodeeringud");
+            test.equal(url, 'http://andrisreinman.com/klikkide-jargi-populaarsuse-arvutamine/');
+            test.equal(fileName, 'index.html');
+            test.equal(contentType, 'text/html');
             test.done();
         });
     }
-}
+};
 
-exports["Remove params tests"] = {
-    "Keep all params 1": function(test){
-        var url = "http://www.andrisreinman.com/test?p1=1&p2=2&p3=3";
+exports['Remove params tests'] = {
+    'Keep all params 1': function(test) {
+        var url = 'http://www.andrisreinman.com/test?p1=1&p2=2&p3=3';
         test.equal(url, resolver.removeParams(url, []));
         test.done();
     },
-    "Keep all params 2": function(test){
-        var url = "http://www.andrisreinman.com/test?p1=1&p2=2&p3=3";
-        test.equal(url, resolver.removeParams(url, ["noexist"]));
+    'Keep all params 2': function(test) {
+        var url = 'http://www.andrisreinman.com/test?p1=1&p2=2&p3=3';
+        test.equal(url, resolver.removeParams(url, ['noexist']));
         test.done();
     },
-    "No params 1": function(test){
-        var url = "http://www.andrisreinman.com/test";
+    'No params 1': function(test) {
+        var url = 'http://www.andrisreinman.com/test';
         test.equal(url, resolver.removeParams(url, []));
         test.done();
     },
-    "No params 1": function(test){
-        var url = "http://www.andrisreinman.com/test";
-        test.equal(url, resolver.removeParams(url, ["noexists"]));
+    'No params 2': function(test) {
+        var url = 'http://www.andrisreinman.com/test';
+        test.equal(url, resolver.removeParams(url, ['noexists']));
         test.done();
     },
-    "Remove string param": function(test){
-        var url = "http://www.andrisreinman.com/test?p1=1&p2=2&p3=3";
-        test.equal("http://www.andrisreinman.com/test?p1=1&p3=3", resolver.removeParams(url, ["p2"]));
+    'Remove string param': function(test) {
+        var url = 'http://www.andrisreinman.com/test?p1=1&p2=2&p3=3';
+        test.equal('http://www.andrisreinman.com/test?p1=1&p3=3', resolver.removeParams(url, ['p2']));
         test.done();
     },
-    "Remove regex param 1": function(test){
-        var url = "http://www.andrisreinman.com/test?p1=1&p2=2&p3=3";
-        test.equal("http://www.andrisreinman.com/test?p1=1&p3=3", resolver.removeParams(url, [/p2/]));
+    'Remove regex param 1': function(test) {
+        var url = 'http://www.andrisreinman.com/test?p1=1&p2=2&p3=3';
+        test.equal('http://www.andrisreinman.com/test?p1=1&p3=3', resolver.removeParams(url, [/p2/]));
         test.done();
     },
-    "Remove regex param 2": function(test){
-        var url = "http://www.andrisreinman.com/test?p1=1&p2=2&p3=3";
-        test.equal("http://www.andrisreinman.com/test", resolver.removeParams(url, [/p/]));
+    'Remove regex param 2': function(test) {
+        var url = 'http://www.andrisreinman.com/test?p1=1&p2=2&p3=3';
+        test.equal('http://www.andrisreinman.com/test', resolver.removeParams(url, [/p/]));
         test.done();
     },
-    "Remove regex and string": function(test){
-        var url = "http://www.andrisreinman.com/test?p1=1&p2=2&p3=3&r=45";
-        test.equal("http://www.andrisreinman.com/test?p3=3", resolver.removeParams(url, [/p1|p2/, "r"]));
+    'Remove regex and string': function(test) {
+        var url = 'http://www.andrisreinman.com/test?p1=1&p2=2&p3=3&r=45';
+        test.equal('http://www.andrisreinman.com/test?p3=3', resolver.removeParams(url, [/p1|p2/, 'r']));
         test.done();
     }
-}
+};
